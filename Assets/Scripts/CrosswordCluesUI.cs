6d99a0ae906509d8596c2ac35b532c26312302e9
Zakarie McCabe <morrowmoss.studios@gmail.com>
2026-01-01 15:45:26 +0700

    hooked up buttons in clues scene

--- Assets/Scripts/CrosswordCluesUI.cs
+++ Assets/Scripts/CrosswordCluesUI.cs
@@ -1,12 +1,19 @@
 using System.Text;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using System.Collections.Generic;
+using UnityEngine.SceneManagement;   // <-- NEW
 
 public class CrosswordCluesUI : MonoBehaviour
 {
+    [Header("Clue Text")]
     [SerializeField] private TMP_Text acrossText;
     [SerializeField] private TMP_Text downText;
+
+    [Header("Scene Names")]          // <-- NEW
+    [SerializeField] private string crosswordSceneName = "CrosswordMode";
+    [SerializeField] private string quitPopupSceneName = "Quit_PopUp";
+    [SerializeField] private string settingsSceneName  = "SettingsScene";
 
     private void Start()
     {
@@ -37,5 +44,22 @@ public class CrosswordCluesUI : MonoBehaviour
 
         if (acrossText != null) acrossText.text = acrossBuilder.ToString();
         if (downText != null)   downText.text   = downBuilder.ToString();
+    }
+
+    // ---------- BUTTON HOOKS (NEW) ----------
+
+    public void OnBackPressed()
+    {
+        SceneManager.LoadScene(crosswordSceneName);
+    }
+
+    public void OnQuitPressed()
+    {
+        SceneManager.LoadScene(quitPopupSceneName);
+    }
+
+    public void OnSettingsPressed()
+    {
+        SceneManager.LoadScene(settingsSceneName);
     }
 }
